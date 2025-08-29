7d1d42a2bc8070b7260ded74139adc57c1f177ab
thonnykjjjkk <thonnyvrc@gmail.com>
2025-08-29 18:38:22 -0300

    set closed

--- src/util/hooks/auth.ts
+++ src/util/hooks/auth.ts
@@ -13,7 +13,7 @@ const pages = {
 export const useAuth = () => {
   const router = useRouter();
 
-  const [fechado, setFechado] = useState(false);
+  const [fechado, setFechado] = useState(true);
   const [authCarregado, setAuthCarregado] = useState(false);
   const [cliente, setCliente] = useState<ICliente>();
   const [pedido, setPedido] = useState<IPedido>();
